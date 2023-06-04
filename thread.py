7507ba06b67c40ea0015ff8677fe5c5f60e48938
Saayan8981 <saayan8981@gmail.com>
2023-06-04 13:23:29 +0530

    UPDATED

--- thread.py
+++ thread.py
@@ -3,15 +3,16 @@ import threading
 import time
 
 class thread_test(object):
+    item = [34, 5, 4, 6, 3, 126, 2, 3, 4, 5, 5, 6, 6, 7, 342, 7, 231, 7, 78, 121, 43, 1, 3, 3, 3, 45, 3, 5, 643, 75, 865, 5, 375, 6, 7, 578, 76]
+    print(len(item))
     
-    item = [34,5,4,6,3,126,2,3,4,5,5,6,6,7,342,7,231,7,78,121,43,1,3,3,3,45,3,5,643,75,865,5,375,6,7,578,76]
-    print (len(item))
-
+    def __init__(self):
+        self.count = 0
+    
     def test(self, item_part):
-        count = 0
-        count += 1
-        print (f'{item_part}')
-        print (f'{count}')
+        self.count += 1
+        print(f'{item_part}')
+        print(f'{self.count}')
         self.q.task_done()
         
     def func(self, q, thread_no_start, thread_no_end):
@@ -27,14 +28,13 @@ class thread_test(object):
 
     def run(self):
         self.q = queue.Queue()
-        gap  = 5
-        for i in range(0,len(self.item),gap):
-            print (i)
-            worker = threading.Thread(target=self.func, args=(self.q,i,i+gap), daemon = True)
+        gap = 5
+        for i in range(0, len(self.item), gap):
+            print(i)
+            worker = threading.Thread(target=self.func, args=(self.q, i, i + gap), daemon=True)
             self.q.put(worker)
             worker.start()
             
         self.q.join()
-        
-if __name__ == '__main__':        
-    thread_test().run()        
+
+thread_test().run()
