c27ea94557793efe00ce6867ceaeab9d786e7037
Sayan8981 <saayan8981@gmail.com>
2024-01-26 20:46:29 +0530

    modified

--- decorator_test.py
+++ decorator_test.py
@@ -40,3 +40,33 @@ def sum(a, b):
 a,b = 3, 4
 
 print ("result is :", sum(a, b))
+
+# Factorial program with memoization using
+# decorators.
+ 
+# A decorator function for function 'f' passed
+# as parameter
+memory = {}
+def memoize_factorial(f):
+     
+    # This inner function has access to memory
+    # and 'f'
+    def inner(num):
+        if num not in memory:
+            memory[num] = f(num)
+            print('result saved in memory')
+        else:
+            print('returning result from saved memory')
+        return memory[num]
+ 
+    return inner
+     
+@memoize_factorial
+def facto(num):
+    if num == 1:
+        return 1
+    else:
+        return num * facto(num-1)
+ 
+print(facto(5))
+print(facto(5)) # directly coming from saved memory
