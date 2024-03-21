b6a3ed01f524647c55e3ecc86959f90f4610878c
Sayan8981 <saayan8981@gmail.com>
2024-03-21 21:09:53 +0530

    modified

--- pyspark_tutorial/pyspark_test.py
+++ pyspark_tutorial/pyspark_test.py
@@ -39,6 +39,36 @@ df_pyspark.describe().show()
 #to get dtypes
 print (df_pyspark.dtypes)
 
+#adding columns 
+df_pyspark = df_pyspark.withColumn('Experience after 2 years', df_pyspark['Experience']+2)
+df_pyspark.show()
+
+#droping columns
+df_pyspark = df_pyspark.drop('Experience after 2 years')
+df_pyspark.show()
+
+#renaming the colums
+df_pyspark.withColumnRenamed('Name', 'new_name').show()
+ 
+#filtering data 
+# Create a DataFrame from a list of tuples
+data = [("Alice", 25), ("Bob", 30), ("Charlie", 35)]
+df = spark.createDataFrame(data, ["Name", "Age"])
+
+# Filter rows where age is greater than 30
+filtered_df = df.filter(df.Age > 30)
+
+# Show the filtered DataFrame
+filtered_df.show()
+
+data_sec = [("Krish", 31, 10, 30000), ("Sunny", 30, 4, None), ("Paul", 24, 3, 20000), ("harsha", 21, 1, 15000), ("Dev", 20, None ,29000), ("Hak", None, 2, None), (None, None, None, 12000), (None, None, None, None), (None, None, None, None)]
+
+df_pyspark_sec = spark.createDataFrame(data_sec, ["Name","Age", "Experience", "Salary"])
+
+df_pyspark_sec = df_pyspark_sec.na.drop(how='any', thresh=2)
+df_pyspark_sec.show()
+
+
  
 #Stop SparkSession
 spark.stop()
