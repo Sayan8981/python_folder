423e158787df61f7a298f6730cffb2612c8a9c61
Sayan8981 <saayan8981@gmail.com>
2024-08-06 12:11:36 +0530

    updated

--- Code/factorial.py
+++ Code/factorial.py
@@ -1,8 +1,19 @@
 
-num = int(input("Enter a number for factorial:"))
-factorial_num = num
+# num = int(input("Enter a number for factorial:"))
+# factorial_num = num
 
-for index in range (num-1,0,-1):
-    factorial_num = factorial_num * index
+# for index in range (num-1,0,-1):
+#     factorial_num = factorial_num * index
 
-print (factorial_num)
+# print (factorial_num)
+import math
+
+def factorial(x):
+    if x <= 1:
+       return 1
+    else:
+        return x * factorial(x-1)
+    
+print (factorial(6))    
+
+print (math.factorial(6))
---
+++ test.py
@@ -0,0 +1,44 @@
+
+
+# defining a decorator  
+def hello_decorator(func):  
+    
+    # inner1 is a Wrapper function in   
+    # which the argument is called  
+        
+    # inner function can access the outer local  
+    # functions like in this case "func"  
+    def inner1():  
+        print("Hello, this is before function execution")  
+    
+        # calling the actual function now  
+        # inside the wrapper function.  
+        func()  
+    
+        print("This is after function execution")  
+            
+    return inner1  
+    
+@hello_decorator    
+# defining a function, to be called inside wrapper  
+def function_to_be_used():  
+    print("This is inside the function !!")  
+    
+    
+# passing 'function_to_be_used' inside the  
+# decorator to control its behavior 
+ 
+#function_to_be_used = hello_decorator(function_to_be_used)  
+    
+    
+# calling the function  
+function_to_be_used()
+
+lst = [1, 2, 3, 4]
+dict_ = {"a":1, "b":2, "c":3, "d":4}
+
+sqr_list = [num**2 for num in lst]
+print("This is the squre list:", sqr_list)
+print (dict_.items())
+dict_list = {num[0]:num[1]**3 for num in dict_.items()}
+print (dict_list)
