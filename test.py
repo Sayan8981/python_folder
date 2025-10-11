c330ee763b3d9ae14449228697bcc989d19ce2e0
Sayan8981 <saayan8981@gmail.com>
2025-10-11 15:20:18 +0530

    updated

--- test.py
+++ test.py
@@ -1,12 +1,31 @@
-def func():
-    test_dict = dict()
-    test_dict["1"] = ("101", "2")
-    test_dict["2"] = ("202", "3")
-    import pdb;pdb.set_trace()
-    top_task = max(
-            test_dict.items(),
-            key=lambda item: (item[1][1], item[0]))
-    
-    return top_task
+list_ = [1,1, 3, 2, 4, 3, 2, 2, 2, 5, 5,88,5,4]
 
-func()
+from collections import Counter
+
+frequency = Counter(list_)
+most_common_element, count = frequency.most_common(1)[0]
+print (f"most_common_ele: {most_common_element} , count : {count}")
+
+
+num = 1232
+
+num = list(str(num))
+print (int("".join(num[::-1])))
+
+class list_node(object):
+    def __init__(self, val = 0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution(object):
+    def hascycle(self, head: list_node) -> bool:
+        slow = head
+        fast = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+            
+        return False    
