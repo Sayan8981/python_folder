8c1d2d095ba3fb0f15e981950b6f305366d838fb
Sayan8981 <saayan8981@gmail.com>
2025-10-11 19:22:49 +0530

    updated

--- test.py
+++ test.py
@@ -34,3 +34,27 @@ array_ = [1, 2, 2, 3, 3, 4, 3]
 print (sorted(array_))
 originl_array = set(sorted(array_))
 print (list(originl_array))
+
+
+
+def profit_list(price_list):
+    profit_dict = dict()
+    #iterate through the price listed 
+    for price_index in range(0,len(price_list)):
+        #to retrive the buy price
+        buy_price = price_list[price_index]
+        #to check the price index is less than the length of the price listed 
+        while price_index < len(price_list) - 1:
+            price_index += 1 #incremental of price index
+            sell_price = price_list[price_index] #to get the sell price index
+            temp_profit = sell_price - buy_price
+            if temp_profit > 0:
+                profit_dict.update ({"%s"%price_index: temp_profit})
+               
+    return profit_dict  
+
+          
+price_list = [10,9,16,17,19,23]#[500,120,130,250,100,80]
+profit_dict = profit_list(price_list=price_list)
+
+print ({str(max(profit_dict,key = lambda x: profit_dict[x])): profit_dict.get(max(profit_dict,key = lambda x: profit_dict[x]))}) 
